485562bea5f9ba58dca4ba5d695147a287560b8c
Martin <martin@ushakiran-MacBook-Pro.local>
2020-01-17 03:12:32 +0100

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+const API = 'https://api.github.com/users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 90,
+      density: {
+        enable: true,
+        value_area: 1000
+      }
+    }
+  }
+}
+      
+
+     
+class App extends Component {
+	constructor(){
+		super()
+		this.state ={
+			gitUsers: [],
+			username: '',
+            searchField:'',
+            input:''
+		}
+	
+	}
+
+	
+		 onsubmit = (event) => {
+		    this.setState({input : event.target.value});
+		    
+		  }
+		  onbuttonOn = () => {
+
+		    this.setState({username: this.state.input});
+				    let url = `${API}/${this.state.input}`;
+				fetch(url).then(response=> {
+					return response.json()
+
+				})
+				.then(users=> {
+					this.setState({ gitUsers: users})
+		            
+				});
+		    console.log('submit');
+		    
+		  }
+	render() {
+		//console.log(this.state.gitUsers);
+		console.log(this.state.gitUsers.login);
+		return (
+			
+			
+			<div className='tc ' >
+           
+                 
+           
+
+		           
+           	<h1 className= 'f1 b--dark-blue'> GitBook </h1>
+              
+               <div className='tc  bg-dark-blue br3 pa3 ma2 dib bw2 shadow-5'>
+                      <div className='pa2'>
+					      <input
+					        className='pa3 ba b--green bg-lightest-blue'
+					        type='search'
+					        placeholder='search users'
+					        onChange={this.onsubmit}
+					      />
+					      <button className=''
+                          onClick ={this.onbuttonOn}
+                          >Detect</button>
+
+					    </div>
+      					<img alt='users' src={this.state.gitUsers.avatar_url} />
+      				<div>
+        			
+        			<p>Username: {this.state.gitUsers.login}</p>
+        			<p>Name: {this.state.gitUsers.name}</p>
+        			<p>Following: {this.state.gitUsers.following}</p>
+        			<p>Repos: {this.state.gitUsers.public_repos}</p>
+
+
+      				</div>
+    			</div>
+           </div>
+          
+			);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
