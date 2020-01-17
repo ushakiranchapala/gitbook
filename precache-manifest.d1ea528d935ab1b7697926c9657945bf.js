b402b15a058e0a76884b45e213b0617106ec0e1b
Martin <martin@ushakiran-MacBook-Pro.local>
2020-01-17 03:27:27 +0100

    Updates

---
+++ precache-manifest.d1ea528d935ab1b7697926c9657945bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a929d71f7ea5d0264e9a5b7db19807a",
+    "url": "/gitbook/index.html"
+  },
+  {
+    "revision": "aadc556bc64ce7e40526",
+    "url": "/gitbook/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b448eeb68f36137380d7",
+    "url": "/gitbook/static/css/main.a03bcf91.chunk.css"
+  },
+  {
+    "revision": "aadc556bc64ce7e40526",
+    "url": "/gitbook/static/js/2.4c97e8ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gitbook/static/js/2.4c97e8ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b448eeb68f36137380d7",
+    "url": "/gitbook/static/js/main.274f085a.chunk.js"
+  },
+  {
+    "revision": "e9060b02f704684e56a8",
+    "url": "/gitbook/static/js/runtime-main.4558a5f8.js"
+  }
+]);
